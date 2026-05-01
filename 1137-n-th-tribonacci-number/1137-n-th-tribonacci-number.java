47aed55da5d381174b2a1083caf552b2207c415e
DHARMESH RAJPUT <unknown2605work@gmail.com>
2026-05-01 22:46:14 +0530

    Time: 0 ms (100%), Space: 41.7 MB (96.31%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int n1 = 0, n2 = 1, n3 = 1;
+
+        for (int i = 3; i <= n; i++) {
+            int n4 = n1 + n2 + n3;
+            n1 = n2;
+            n2 = n3;
+            n3 = n4;
+        }
+
+        return n3;
+    }
+}
