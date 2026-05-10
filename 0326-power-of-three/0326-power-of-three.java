fed65e2f137d9c22322fcf5c686ff03f76d88cbc
DHARMESH RAJPUT <unknown2605work@gmail.com>
2026-05-10 22:10:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n <= 0){
+        if(n == 1) {
+            return true;
+        }
+
+        if(n <= 0 || n % 3 != 0) {
             return false;
         }
 
-        while(n % 3 == 0){
-            n = n / 3;
-        }
-
-        return n == 1;
+        return isPowerOfThree(n / 3);
     }
 }
